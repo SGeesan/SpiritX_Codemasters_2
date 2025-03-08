c4d070ba40a9c3b3ef581dbe802e1a1821c43298
vinukab <vinukabuddhima32@gmail.com>
2025-03-08 23:21:39 +0530

    cleaned the unnecesssary code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,64 +1,16 @@
-// src/App.jsx
-import { useState, useEffect } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
-import AdminLogin from "./components/AdminLogin";
-import AdminPanel from "./pages/AdminPage";
-import { SocketProvider } from "./contexts/SocketContext";
+
+import './App.css'
+import AdminPage from './pages/AdminPage';
+
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  // Check if admin is already logged in using token
-  useEffect(() => {
-    const token = localStorage.getItem("adminToken");
-    if (token) {
-      setIsAuthenticated(true);
-    }
-  }, []);
-
-  const handleLogin = () => {
-    setIsAuthenticated(true);
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem("adminToken");
-    setIsAuthenticated(false);
-  };
+  
 
   return (
-    <SocketProvider>
-      <Router>
-        <Routes>
-          <Route
-            path="/login"
-            element={
-              isAuthenticated ? (
-                <Navigate to="/admin" />
-              ) : (
-                <AdminLogin onLogin={handleLogin} />
-              )
-            }
-          />
-          <Route
-            path="/admin/*"
-            element={
-              isAuthenticated ? (
-                <AdminPanel onLogout={handleLogout} />
-              ) : (
-                <Navigate to="/login" />
-              )
-            }
-          />
-          <Route path="*" element={<Navigate to="/login" />} />
-        </Routes>
-      </Router>
-    </SocketProvider>
+    <>
+      
+    </>
   );
 }
 
-export default App;
+export default App
