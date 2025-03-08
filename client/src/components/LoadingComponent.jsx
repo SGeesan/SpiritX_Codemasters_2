fff06eef65fec4ab5c6656de997424098b093517
vinukab <vinukabuddhima32@gmail.com>
2025-03-09 00:00:10 +0530

    added a loading component

---
+++ client/src/components/LoadingComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+export const LoadingComponent = () => {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/37c3c53d-4af1-4486-a2d6-6c3fc55faf68/4Nv2viAAB8.lottie"
+      loop
+      autoplay
+    />
+  );
+}
+
+export default LoadingComponent;
